cecfa3ef3efbd23a4ffdc9083aa4c09f56915a8f
DevValmax <yan.tokaruk@gmail.com>
2024-07-25 22:50:31 +0300

    add project section

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,3 +1,19 @@
 export const projects = Swiper => {
-  console.log(Swiper);
+  const section = document.querySelector('.projects');
+  if (section) {
+    const slider = section.querySelector('.projects-js-list');
+
+    const swiper = new Swiper(slider, {
+      speed: 600,
+      navigation: {
+        nextEl: section.querySelector('.swiper-button-next'),
+        prevEl: section.querySelector('.swiper-button-prev'),
+      },
+      keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+      },
+      allowTouchMove: true,
+    });
+  }
 };
