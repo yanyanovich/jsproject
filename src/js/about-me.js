19d87a233041b90530be8ab2c4f284b571ac1e92
Your Name <you@example.com>
2024-07-26 21:49:24 +0300

    add the accordion code

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,6 +1,35 @@
-// import Accordion from 'accordion-js';
-// import 'accordion-js/dist/accordion.min.css';
-
 export const aboutMe = Swiper => {
-  console.log(Swiper);
+  const triggers = document.querySelectorAll('.question-js');
+  const answers = document.querySelectorAll('.answer-js');
+  answers.forEach(answer => {
+    answer.style.maxHeight = answer.scrollHeight + 'px';
+  });
+  triggers.forEach(trigger => {
+    let triggerSiblings = trigger
+      .closest('.item-js')
+      .querySelectorAll('.question-js');
+    let contents = trigger.closest('.item-js').querySelectorAll('.answer-js');
+    trigger.addEventListener('click', e => {
+      triggerSiblings.forEach(triggerSibling => {
+        if (e.target.closest('.question-js') !== triggerSibling) {
+          triggerSibling.classList.remove('active');
+        }
+      });
+      contents.forEach(content => {
+        if (e.target.closest('.question-js').nextElementSibling !== content) {
+          content.style.maxHeight = '0px';
+          content.classList.add('hidden');
+        } else {
+          if (content.classList.contains('hidden')) {
+            content.style.maxHeight = content.scrollHeight + 'px';
+          } else {
+            content.style.maxHeight = '0px';
+          }
+          content.classList.toggle('hidden');
+        }
+      });
+      trigger.classList.toggle('active');
+      trigger.classList.remove('hover');
+    });
+  });
 };
