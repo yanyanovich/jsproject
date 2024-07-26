02e0b49e454c076fb3a3237b8ceaa189b242d68d
Businda84 <109301774+Businda84@users.noreply.github.com>
2024-07-26 21:58:33 +0300

    reviews markup

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -4,3 +4,30 @@ export const reviews = Swiper => {
 
   ajaxRequest();
 };
+
+const reviewsEl = document.querySelector('.swiper_reviews');
+
+fetch('https://portfolio-js.b.goit.study/api/reviews')
+  .then(res => {
+    if (!res.ok) {
+      throw new Error(res.status);
+    }
+    return res.json();
+  })
+  .then(data => {
+    console.log(data);
+    const listMarkUp = createListMarkUp(data);
+    reviewsEl.insertAdjacentHTML('beforeend', listMarkUp);
+  })
+  .catch(err => {
+    console.error('Service not found');
+  });
+
+const createLiMarkUp = ({
+  author,
+  avatar_url,
+  review,
+}) => `<img class="js-reviews-photo" src="${avatar_url}" alt="${author}" />
+      <h3 class="js-reviews-name">${author}</h3>
+      <p class="js-user-review">${review}</p>`;
+const createListMarkUp = arrReviews => arrReviews.map(createLiMarkUp).join('');
